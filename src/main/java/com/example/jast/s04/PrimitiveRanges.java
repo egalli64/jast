ac63f9f7e468152e71b0a72d2295ff99191d753c
Emanuele <egalli64@gmail.com>
2022-03-06 18:59:02 +0100

    Ranges

---
+++ src/main/java/com/example/jast/s04/PrimitiveRanges.java
@@ -0,0 +1,14 @@
+package com.example.jast.s04;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class PrimitiveRanges {
+    public static void main(String[] args) {
+        System.out.println("Right open range on primitive int stream [40..43)");
+        IntStream.range(40, 43).forEach(System.out::println);
+
+        System.out.println("Closed range on primitive long stream [40..43)");
+        LongStream.rangeClosed(Long.MAX_VALUE - 2, Long.MAX_VALUE).forEach(System.out::println);
+    }
+}
