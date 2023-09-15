7fc3a4c60d9eb310e7b830a09981bbfe85e32fb9
Emanuele <egalli64@gmail.com>
2023-09-15 20:04:47 +0200

    more comments

--- src/main/java/com/example/jast/s04/SimpleTerminal.java
+++ src/main/java/com/example/jast/s04/SimpleTerminal.java
@@ -1,7 +1,15 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
 package com.example.jast.s04;
 
 import java.util.List;
 
+/**
+ * Terminal operations: forEach and count
+ */
 public class SimpleTerminal {
     public static void main(String[] args) {
         List<Integer> values = List.of(11, 34, 2);
--- src/main/java/com/example/jast/s04/SimpleTerminalPrimitive.java
+++ src/main/java/com/example/jast/s04/SimpleTerminalPrimitive.java
@@ -1,7 +1,15 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
 package com.example.jast.s04;
 
 import java.util.Arrays;
 
+/**
+ * Terminal operations for primitive stream: forEach and count
+ */
 public class SimpleTerminalPrimitive {
     public static void main(String[] args) {
         int[] values = { 11, 34, 2 };
