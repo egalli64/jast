67da506c1fa9d46da22e7c8b7823c9a5e0c23557
Emanuele <egalli64@gmail.com>
2023-09-16 11:24:13 +0200

    more comments

--- src/main/java/com/example/jast/s08/Pair.java
+++ src/main/java/com/example/jast/s08/Pair.java
@@ -1,4 +1,12 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
 package com.example.jast.s08;
 
+/**
+ * A two-integer tuple
+ */
 record Pair(int first, int second) {
 }
