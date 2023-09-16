03e966a2cfc1689b52e6b9e5623357139a5cee60
Emanuele <egalli64@gmail.com>
2023-09-16 12:39:05 +0200

    more comment

--- src/main/java/com/example/jast/s12/Reducing.java
+++ src/main/java/com/example/jast/s12/Reducing.java
@@ -1,9 +1,18 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
 package com.example.jast.s12;
 
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * Plain usage example for collect using the collector reducing, compare it with
+ * using reduce
+ */
 public class Reducing {
     public static void main(String[] args) {
         List<Integer> values = List.of(4, 2, 5, -23, 54, -11, 67, -100, 15, -83, 45);
