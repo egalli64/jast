d418357f9176d5e01cbc15adf3c9bd745a01d3a3
Emanuele <egalli64@gmail.com>
2023-09-17 09:57:51 +0200

    more comments

--- src/main/java/com/example/jast/s13/Weight.java
+++ src/main/java/com/example/jast/s13/Weight.java
@@ -1,5 +1,13 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
 package com.example.jast.s13;
 
+/**
+ * Dogs are partitioned in these categories
+ */
 enum Weight {
     LIGHT, MEDIUM, HEAVY
 }
