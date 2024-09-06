ab3a86b68c481d4bc7dcc3bf5fa65b3c73732988
Emanuele <egalli64@gmail.com>
2024-09-06 10:56:00 +0200

    review

--- src/main/java/com/example/jast/m1/s3/StreamFactories.java
+++ src/main/java/com/example/jast/m1/s3/StreamFactories.java
@@ -3,28 +3,28 @@
  * 
  * https://github.com/egalli64/jast
  */
-package com.example.jast.s02;
+package com.example.jast.m1.s3;
 
 import java.util.stream.Stream;
 
 /**
- * Create a stream from a sequence; an empty stream, a stream with a single
- * element or empty (so to discard null)
+ * Three stream factory methods
  */
-public class Creation {
+public class StreamFactories {
     public static void main(String[] args) {
         System.out.println("Print each element in the stream of 12, 34, 2");
         Stream.of(12, 34, 2).forEach(System.out::println);
         System.out.println("---");
 
-        System.out.println("Print each element in the empty stream");
+        System.out.println("An empty stream performs no processing!");
         Stream.empty().forEach(System.out::println);
         System.out.println("---");
 
         String[] elements = { "Hello", null };
         for (String element : elements) {
-            System.out.println("Print each element in the stream created from an element possibly null");
-            Stream.ofNullable(element).forEach(System.out::println);
+            System.out.println("A nullable stream has one or no elements:");
+            Stream<String> stream = Stream.ofNullable(element);
+            stream.forEach(System.out::println);
             System.out.println("---");
         }
     }
