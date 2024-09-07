3a868b28a20b0be03a4037ac8dc02a46b4d01813
Emanuele <egalli64@gmail.com>
2024-09-07 17:31:36 +0200

    matching terminals

---
+++ src/main/java/com/example/jast/m1/s8/Matching.java
@@ -0,0 +1,30 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
+package com.example.jast.m1.s8;
+
+import java.util.List;
+
+/**
+ * Terminal operations: allMatch, anyMatch, noneMatch
+ */
+public class Matching {
+    public static void main(String[] args) {
+        List<Integer> data = List.of(4, 2, 5, 23, 54, 11, 67, 100, 15, 83, 45);
+        System.out.println(data);
+
+        if (data.stream().allMatch(x -> x > 0)) {
+            System.out.println("All the elements are positive");
+        }
+
+        if (data.stream().anyMatch(x -> x % 2 == 0)) {
+            System.out.println("There is at least an even element");
+        }
+
+        if (data.stream().noneMatch(x -> x > 100)) {
+            System.out.println("No element is bigger than 100");
+        }
+    }
+}
