de35405b16cb0f48bb82abe1b0b317fc61583058
Emanuele <egalli64@gmail.com>
2024-09-07 16:02:40 +0200

    change slide

---
+++ src/main/java/com/example/jast/m1/s7/PositionalFiltering.java
@@ -0,0 +1,29 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
+package com.example.jast.m1.s7;
+
+import java.util.List;
+
+/**
+ * Intermediate filtering operations by position: limit, skip
+ */
+public class PositionalFiltering {
+    public static void main(String[] args) {
+        List<Integer> values = List.of(1, 3, 2, 1, 5, 2);
+        System.out.println("Original data: " + values);
+
+        System.out.println("filter() the even values:");
+        values.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("filterWhile() odd values, drop from the first even value on:");
+        values.stream().takeWhile(x -> x % 2 != 0).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("dropWhile() odd values, keep from the first even value on:");
+        values.stream().dropWhile(x -> x % 2 != 0).forEach(System.out::println);
+    }
+}
