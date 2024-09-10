51fa230e726cb7940d252669812b9b7195dc1916
Emanuele <egalli64@gmail.com>
2024-09-10 22:07:47 +0200

    review

--- src/main/java/com/example/jast/m3/s3/bean/DogReducing.java
+++ src/main/java/com/example/jast/m3/s3/bean/DogReducing.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jast
  */
-package com.example.jast.s12;
+package com.example.jast.m3.s3.bean;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,9 +11,9 @@ import java.util.stream.Collectors;
 import com.example.jast.dto.bean.DogExt;
 
 /**
- * Working with specialized reducing collectors (JavaBean based)
+ * Working on JavaBeans with specialized reducing collectors
  */
-public class DogReducingBean {
+public class DogReducing {
     public static void main(String[] args) {
         List<DogExt> dogs = List.of( //
                 new DogExt("Bob", "Tom Hanks", 7, 12.5), //
@@ -22,28 +22,21 @@ public class DogReducingBean {
         );
 
         System.out.println("Dogs: " + dogs);
-        System.out.println("---");
 
-        System.out.print("Sum weights by reducing collector: ");
-        System.out.println(dogs.stream().collect(Collectors.reducing(0.0, DogExt::getWeight, Double::sum)));
-
-        System.out.print("Same, using summingDouble collector: ");
+        System.out.print("Sum weights by summingDouble collector: ");
         System.out.println(dogs.stream().collect(Collectors.summingDouble(DogExt::getWeight)));
         System.out.print("Same, by map-reduce with DoubleStream::sum(): ");
         System.out.println(dogs.stream().mapToDouble(DogExt::getWeight).sum());
         System.out.println("---");
 
-        System.out.print("Average age by reducing collector: ");
-        int sum = dogs.stream().collect(Collectors.reducing(0, DogExt::getAge, Integer::sum));
-        System.out.println(dogs.isEmpty() ? 0 : sum / (double) dogs.size());
-
-        System.out.print("Same, using averagingInt collector: ");
+        System.out.print("Average age by averagingInt collector: ");
         System.out.println(dogs.stream().collect(Collectors.averagingInt(DogExt::getAge)));
         System.out.print("Same, by map-reduce with IntStream::average(): ");
         dogs.stream().mapToInt(DogExt::getAge).average().ifPresent(System.out::println);
         System.out.println("---");
 
-        System.out.println("All owners: " + dogs.stream().map(DogExt::getOwner).collect(Collectors.joining(", ")));
+        System.out.println("All owners: " + //
+                dogs.stream().map(DogExt::getOwner).collect(Collectors.joining(", ")));
         System.out.print("All owners in brackets: ");
         System.out.println(dogs.stream().map(DogExt::getOwner).collect(Collectors.joining(", ", "[", "]")));
     }
--- src/main/java/com/example/jast/m3/s3/rec/DogReducing.java
+++ src/main/java/com/example/jast/m3/s3/rec/DogReducing.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jast
  */
-package com.example.jast.s12;
+package com.example.jast.m3.s3.rec;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 import com.example.jast.dto.rec.DogExt;
 
 /**
- * Working with specialized reducing collectors
+ * Working on records with specialized reducing collectors
  */
 public class DogReducing {
     public static void main(String[] args) {
@@ -22,29 +22,22 @@ public class DogReducing {
         );
 
         System.out.println("Dogs: " + dogs);
-        System.out.println("---");
 
-        System.out.print("Sum weights by reducing collector: ");
-        System.out.println(dogs.stream().collect(Collectors.reducing(0.0, DogExt::weight, Double::sum)));
-
-        System.out.print("Same, using summingDouble collector: ");
+        System.out.print("Sum weights by summingDouble collector: ");
         System.out.println(dogs.stream().collect(Collectors.summingDouble(DogExt::weight)));
         System.out.print("Same, by map-reduce with DoubleStream::sum(): ");
         System.out.println(dogs.stream().mapToDouble(DogExt::weight).sum());
         System.out.println("---");
 
-        System.out.print("Average age by reducing collector: ");
-        int sum = dogs.stream().collect(Collectors.reducing(0, DogExt::age, Integer::sum));
-        System.out.println(dogs.isEmpty() ? 0 : sum / (double) dogs.size());
-
-        System.out.print("Same, using averagingInt collector: ");
+        System.out.print("Average age by averagingInt collector: ");
         System.out.println(dogs.stream().collect(Collectors.averagingInt(DogExt::age)));
         System.out.print("Same, by map-reduce with IntStream::average(): ");
         dogs.stream().mapToInt(DogExt::age).average().ifPresent(System.out::println);
         System.out.println("---");
 
-        System.out.println("All owners: " + dogs.stream().map(DogExt::owner).collect(Collectors.joining(", ")));
+        System.out.println("All owners: " + //
+                dogs.stream().map(x -> x.dog().owner()).collect(Collectors.joining(", ")));
         System.out.print("All owners in brackets: ");
-        System.out.println(dogs.stream().map(DogExt::owner).collect(Collectors.joining(", ", "[", "]")));
+        System.out.println(dogs.stream().map(x -> x.dog().owner()).collect(Collectors.joining(", ", "[", "]")));
     }
 }
