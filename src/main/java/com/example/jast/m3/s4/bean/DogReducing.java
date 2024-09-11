012a66239209e9b5d30d98cd3da3617c11d64106
Emanuele <egalli64@gmail.com>
2024-09-11 11:36:12 +0200

    review

---
+++ src/main/java/com/example/jast/m3/s4/bean/DogReducing.java
@@ -0,0 +1,29 @@
+/*
+ * Introduction to Stream in Java
+ * 
+ * https://github.com/egalli64/jast
+ */
+package com.example.jast.m3.s4.bean;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.jast.dto.bean.DogExt;
+
+/**
+ * Working on JavaBeans with specialized reducing collectors
+ */
+public class DogReducing {
+    public static void main(String[] args) {
+        List<DogExt> dogs = List.of( //
+                new DogExt("Bob", "Tom Hanks", 7, 12.5), //
+                new DogExt("Tom", "Bob Marley", 5, 4.3), //
+                new DogExt("Kim", "Wim Wenders", 4, 8.1) //
+        );
+
+        System.out.println("Dogs: " + dogs);
+
+        System.out.print("Dogs total weight (by reducing collector) is ");
+        System.out.println(dogs.stream().collect(Collectors.reducing(0.0, DogExt::getWeight, Double::sum)));
+    }
+}
